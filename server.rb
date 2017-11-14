5fe40fe83e2b62727c08d78816913375e12f8c34
Sabrina Moczulski <sabrina@belasis.com>
2017-11-14 14:39:35 -0200

    Criação de get

---
+++ server.rb
@@ -0,0 +1,36 @@
+
+require "sinatra"
+
+get '/hello-world' do 
+	name = "Sabrina"
+	"Hello World! #{name} #{rand(20)}"
+end
+
+get "/oi" do
+	erb :oi
+end
+
+get "/new" do
+	@title = "tela de cadastro"
+	erb :edit
+end
+
+post "/new" do
+	"Meu nome é: #{params[:name]}<br>Meu email é: #{params[:email]}"
+end
+
+get "/edit" do
+	@title = "tela de edição"
+	erb :edit
+end
+
+get "/list" do
+	@title = "tela de listagem"
+	erb :list
+end
+
+get "/view" do
+	@title = "tela de visualização"
+	erb :view
+end
+
